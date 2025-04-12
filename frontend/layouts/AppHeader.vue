bd53281dd154c792ec968b53ae984e0a7598d4d9
4dX@#82L <btcdan789@gmail.com>
2025-04-12 14:35:51 +0700

    done logo

--- frontend/layouts/AppHeader.vue
+++ frontend/layouts/AppHeader.vue
@@ -1,48 +1,50 @@
 <template>
   <!-- Header cho người dùng -->
-  <header v-if="!isAdminPage" class="fixed w-full bg-black p-2">
+  <header v-if="!isAdminPage" class="fixed w-full bg-black p-1">
     <div class="flex w-full items-center">
       <!-- Logo -->
-      <div class="w-1/4 flex justify-center items-start">
-      <router-link to="/">
-        <img src="/images/logo/logo.webp" alt="Logo" class="h-12 cursor-pointer" />
-      </router-link>
-    </div>
-      <!-- Menu/Dropdowns -->
-      <div class="w-3/4 flex justify-center items-center space-x-4">
-      <!-- Bộ sưu tập Dropdown -->
-      <div class="dropdown-container">
-        <button class="text-white px-4 py-2">
-          <router-link to="/products">Bộ sưu tập</router-link>
-        </button>
-        <div class="dropdown-menu">
-          <router-link to="/products" class="block px-4 py-2 hover:bg-gray-200">Sản phẩm bán chạy</router-link>
-          <router-link to="/products" class="block px-4 py-2 hover:bg-gray-200">Sản phẩm mới nhất</router-link>
-          <router-link to="/products" class="block px-4 py-2 hover:bg-gray-200">Sản phẩm giảm giá</router-link>
-        </div>
-      </div>
-
-      <!-- Tài khoản Dropdown -->
-      <div class="dropdown-container">
-        <button class="text-white px-4 py-2">
-          <router-link to="/admin">Tài khoản</router-link>
-        </button>
-        <div class="dropdown-menu">
-          <router-link to="/admin" class="block px-4 py-2 hover:bg-gray-200">Thông tin tài khoản</router-link>
-          <router-link to="/login" class="block px-4 py-2 hover:bg-gray-200">Đăng nhập</router-link>
-          <router-link to="/" class="block px-4 py-2 hover:bg-gray-200">Đăng xuất</router-link>
-        </div>
-      </div>
-
-      <!-- Giỏ hàng -->
-      <div>
-        <router-link to="/products" class="text-white px-4 py-2 hover:bg-gray-700 rounded">
-          Giỏ hàng
+      <div class="w-1/4 flex justify-center items-center">
+        <router-link to="/">
+          <img src="/images/logo/logo.webp" alt="Logo"
+            class="h-8 sm:h-8 md:h-14 lg:h-16 xl:h-20 object-contain cursor-pointer" />
         </router-link>
       </div>
+
+      <!-- Menu/Dropdowns -->
+      <div class="w-3/4 flex justify-center items-center space-x-4">
+        <!-- Bộ sưu tập Dropdown -->
+        <div class="dropdown-container">
+          <button class="text-white px-4 py-2">
+            <router-link to="/products">Bộ sưu tập</router-link>
+          </button>
+          <div class="dropdown-menu">
+            <router-link to="/products" class="block px-4 py-2 hover:bg-gray-200">Sản phẩm bán chạy</router-link>
+            <router-link to="/products" class="block px-4 py-2 hover:bg-gray-200">Sản phẩm mới nhất</router-link>
+            <router-link to="/products" class="block px-4 py-2 hover:bg-gray-200">Sản phẩm giảm giá</router-link>
+          </div>
+        </div>
+
+        <!-- Tài khoản Dropdown -->
+        <div class="dropdown-container">
+          <button class="text-white px-4 py-2">
+            <router-link to="/admin">Tài khoản</router-link>
+          </button>
+          <div class="dropdown-menu">
+            <router-link to="/admin" class="block px-4 py-2 hover:bg-gray-200">Thông tin tài khoản</router-link>
+            <router-link to="/login" class="block px-4 py-2 hover:bg-gray-200">Đăng nhập</router-link>
+            <router-link to="/" class="block px-4 py-2 hover:bg-gray-200">Đăng xuất</router-link>
+          </div>
+        </div>
+
+        <!-- Giỏ hàng -->
+        <div>
+          <router-link to="/products" class="text-white px-4 py-2 hover:bg-gray-700 rounded">
+            Giỏ hàng
+          </router-link>
+        </div>
+      </div>
     </div>
-  </div>
-</header>
+  </header>
 
   <!-- Header cho trang Admin -->
   <header v-else class="fixed top-0 left-0 w-full bg-black shadow-md z-50 p-2">
--- frontend/src/user/components/Products.vue
+++ frontend/src/user/components/Products.vue
@@ -14,14 +14,14 @@
     </div>
 
     <h2 class="text-3xl font-bold text-gray-800 mb-8 text-center">Danh sách sản phẩm</h2>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       <div v-for="product in filteredProducts" :key="product._id"
         class="bg-white rounded-xl shadow-md overflow-hidden flex flex-col items-center p-4">
-        <img :src="product.image" alt="Hình ảnh" class="w-full h-48 object-cover mb-4 rounded-md" />
+        <img :src="product.image" alt="Hình ảnh" class="w-full h-120 object-cover mb-4 rounded-md" />
         <h3 class="text-lg font-semibold text-gray-900 mb-1 text-center">{{ product.name }}</h3>
         <p class="text-sm text-gray-600 mb-2 text-center">{{ product.description }}</p>
         <p class="text-red-600 font-bold text-center mb-4">Giá: {{ product.price }} VNĐ</p>
-        <button @click="deleteProduct(product._id)"
+        <button
           class="bg-blue-500 hover:bg-red-600 text-white text-sm px-4 py-2 rounded transition duration-200">
           <router-link class="text-white-500 font-bold" :to="'/products/' + product._id">Xem chi tiết</router-link>
         </button>
@@ -50,15 +50,6 @@ const fetchProducts = async () => {
 
 onMounted(fetchProducts)
 
-const deleteProduct = async (id) => {
-  try {
-    await axios.delete(`/products/${id}`)
-    alert('Xóa sản phẩm thành công!')
-    fetchProducts()
-  } catch (error) {
-    console.error('Lỗi khi xóa sản phẩm:', error)
-  }
-}
 
 const clearFilters = () => {
   searchQuery.value = ''
