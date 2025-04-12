e760ad62539f86bf465920fc12ad50587038643d
4dX@#82L <btcdan789@gmail.com>
2025-04-12 11:40:43 +0700

    chia lại bố cục header

--- frontend/layouts/AppHeader.vue
+++ frontend/layouts/AppHeader.vue
@@ -1,29 +1,15 @@
 <template>
   <!-- Header cho người dùng -->
-  <header v-if="!isAdminPage" class="fixed top-0 left-0 w-full h-16 bg-black text-white z-50 p-4 shadow-md">
-    <div class="container mx-auto flex justify-between items-center">
+  <header v-if="!isAdminPage" class="fixed w-full bg-black p-2">
+    <div class="flex w-full items-center">
       <!-- Logo -->
-      <div class="w-1/4 flex justify-center">
-        <router-link to="/">
-          <img src="/images/logo/logo.webp" alt="Logo" class="h-12 cursor-pointer" />
-        </router-link>
-      </div>
-
-      <!-- Search Bar -->
-      <div class="flex-1 mx-4">
-        <div class="relative">
-          <input type="text" placeholder="Search for items..."
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring" />
-          <button class="absolute right-2 top-2 text-gray-500">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M21 21l-4.35-4.35M17 10a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-          </button>
-        </div>
-      </div>
-
+      <div class="w-1/4 flex justify-center items-start">
+      <router-link to="/">
+        <img src="/images/logo/logo.webp" alt="Logo" class="h-12 cursor-pointer" />
+      </router-link>
+    </div>
+      <!-- Menu/Dropdowns -->
+      <div class="w-3/4 flex justify-center items-center space-x-4">
       <!-- Bộ sưu tập Dropdown -->
       <div class="dropdown-container">
         <button class="text-white px-4 py-2">
@@ -50,12 +36,13 @@
 
       <!-- Giỏ hàng -->
       <div>
-        <button class="container text-white">
-          <router-link to="/products">Giỏ hàng</router-link>
-        </button>
+        <router-link to="/products" class="text-white px-4 py-2 hover:bg-gray-700 rounded">
+          Giỏ hàng
+        </router-link>
       </div>
     </div>
-  </header>
+  </div>
+</header>
 
   <!-- Header cho trang Admin -->
   <header v-else class="fixed top-0 left-0 w-full bg-black shadow-md z-50 p-2">
